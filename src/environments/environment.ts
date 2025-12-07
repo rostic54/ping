3e1694cb5d73860b00fc6b1efab8b5756d552969
rostic54 <rostic54@gmail.com>
2025-12-07 19:53:33 +0200

    fix: set server url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://admirable-phoenix-5ef4f3.netlify.app/api' // Replace with your actual production API URL
+  apiUrl: 'https://pingme-ruvs.onrender.com/api' // Replace with your actual production API URL
 };
